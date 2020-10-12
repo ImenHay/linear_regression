2ab1e5de21a1c6fcea802cf97fc1a45880f8941d
ImenHay <imhaimou@student.42.fr>
2020-10-12 16:15:23 +0200

    problem: prix negatif si km bas

--- predict.py
+++ predict.py
@@ -3,22 +3,23 @@ import os
 import numpy as np
 
 
-
 def get_params():
     params_file = "params.txt"
     try:
-        with open(params_file, "r") as f:
+        with open(params_file, 'r') as f:
             lines = f.readlines()
         try:
-            theta0 = int(lines[0])
-            theta1 = int(lines[1])
+            a = float(lines[0])
+            b = float(lines[1])
+            
         except:
             print("Unvalid file")
             sys.exit()
     except:
         print("theta0 and theta1 are null")
         sys.exit()
-    return np.array([[theta0], [theta1]])
+    return np.array([[a], [b]])
+        
         
 calc = lambda a, b, km: (a * km) + b
 
@@ -26,6 +27,10 @@ calc = lambda a, b, km: (a * km) + b
 if __name__ == "__main__":
     try: 
         theta = get_params()
+        t0 = float(theta[0])
+        t1 = float(theta[1])
+        print(t0)
+        print(t1)
         pass
     except:
         theta = np.array([[0], [0]])
@@ -39,4 +44,4 @@ if __name__ == "__main__":
             break 
         except:
             pass
-    print("This car worth {} euros".format(calc(int(theta[0]), int(theta[1]), km)))
+    print("This car worth {} euros".format(calc(t0, t1, km)))
